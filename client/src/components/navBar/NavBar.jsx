8ed38c9ea30ec88f88ed7aa8c4b7b6c13737b0e9
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2024-07-17 23:44:06 +0100

    update finish navBar styling

--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -1,21 +1,29 @@
 import './navBar.css';
 import React from 'react'
 import { Link } from "react-router-dom";
+import { IoHome } from "react-icons/io5";
+import { IoLogInSharp } from "react-icons/io5";
 
 const NavBar = () => {
   return (
     <div className='nav-container'>
-      <ul>
+      <ul className='navBar'>
             <li>
-            <Link to="/">
-                  Home
-            </Link>
+              <Link to="/">
+                <div className='nav-item'>
+                  <IoHome />
+                  <span>Home</span>
+                </div>
+              </Link>
             </li>
 
             <li>
-            <Link to="/checkIn">
-                  CheckIn
-            </Link>
+              <Link to="/checkIn">
+                <div className='nav-item'>
+                  <IoLogInSharp />
+                  <span>CheckIn</span>
+                </div>
+              </Link>
             </li>
       </ul>
     </div>
