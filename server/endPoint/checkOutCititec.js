d7c0f378543abbb03e4dee92a5e615d5ff483a8d
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2024-07-22 19:51:02 +0100

    update create check in/out for ctititec
    create 2 endpoint in/out
    and
    create 2 function for check in/out button

---
+++ server/endPoint/checkOutCititec.js
@@ -0,0 +1,29 @@
+const pool = require("../database/data");
+
+const checkOutCititec = async (req, res) => {
+  const { slack_user } = req.body;
+
+  if (!slack_user) {
+    return res.status(400).json({ error: "Name is required" });
+  }
+  const query = `UPDATE cititec_keyHolders
+                      SET status = 'out'
+                      WHERE slack_user = $1 AND status = 'in'
+                      RETURNING *`;
+  try {
+    const result = await pool.query(query, [slack_user]);
+
+    if (result.rowCount === 0) {
+      return res
+        .status(404)
+        .json({ error: "No matching entry found or already checked in" });
+    }
+
+    res.json({ message: "Status updated to in", entry: result.rows[0] });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+module.exports = checkOutCititec;
--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-// create express instance with default configuration 
-const cron = require('node-cron');
+// create express instance with default configuration
+const cron = require("node-cron");
 const cors = require("cors");
 const port = process.env.PORT || 3099;
 const pool = require("./database/data");
@@ -27,14 +27,18 @@ const postFormData = require("./endPoint/postFormData");
 const getFormData = require("./endPoint/getFormData");
 const formCheckOut = require("./endPoint/formCheckOut");
 const getDataForCititec = require("./endPoint/cititecData");
-const deleteFormData = require('./endPoint/deleteFormData');
+const deleteFormData = require("./endPoint/deleteFormData");
+const checkInCititec = require("./endPoint/checkInCititec");
+const checkOutCititec = require("./endPoint/checkOutCititec");
 
 // run the endpoint into the server
 app.get("/data", getData);
 app.put("/checkIn", checkIn);
 app.put("/checkOut", checkOut);
-app.post('/submit', postFormData);
-app.get('/formData', getFormData);
-app.put('/formCheckOut', formCheckOut);
-app.get('/getDataForCititec', getDataForCititec);
-app.delete('/delete', deleteFormData);
+app.post("/submit", postFormData);
+app.get("/formData", getFormData);
+app.put("/formCheckOut", formCheckOut);
+app.get("/getDataForCititec", getDataForCititec);
+app.delete("/delete", deleteFormData);
+app.put("/checkInCititec", checkInCititec);
+app.put("/checkOutCititec", checkOutCititec);
